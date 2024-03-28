0cbf018da443ca3b79b45b14d9d24186ac598683
jayendramadaram <dimebeatengreen8@gmail.com>
2024-03-28 13:06:24 +0530

    chore: update p2wpkh for single | anyonecanpay

--- internal/mempool/validate.go
+++ internal/mempool/validate.go
@@ -74,7 +74,7 @@ func (t *Transaction) ValidateTxScripts() error {
 
 			lastSigByte := Signature[len(Signature)-1]
 
-			MessageHash := generateMessageHashSegwit(tempTx, input, lastSigByte)
+			MessageHash := generateMessageHashSegwit(tempTx, i, input, lastSigByte)
 
 			err = ECVerify(MessageHash, Signature, pubKeyHash)
 
@@ -182,7 +182,7 @@ func generateMessageHashLegacy(tempTx Transaction, i int, input TxIn, sigHash by
 	return chainhash.DoubleHashB(serializedTx)
 }
 
-func generateMessageHashSegwit(tempTx Transaction, input TxIn, sigHash byte) []byte {
+func generateMessageHashSegwit(tempTx Transaction, pos int, input TxIn, sigHash byte) []byte {
 	var serializedTx []byte
 	switch sigHash {
 	case 0x01:
@@ -191,8 +191,13 @@ func generateMessageHashSegwit(tempTx Transaction, input TxIn, sigHash byte) []b
 		// tempTx.Vin = []TxIn{input}
 		// tempTx.Vin[0].ScriptSig = input.Prevout.ScriptPubKey
 		serializedTx = SegwitSerializeAllAnyOne(tempTx, input, []byte{0x81, 0x00, 0x00, 0x00})
+	case 0x83:
+		serializedTx = SegwitSerializeSingleAnyOne(tempTx, pos, input, []byte{0x83, 0x00, 0x00, 0x00})
 	default:
 		serializedTx = SegwitSerializeAll(tempTx, input, []byte{0x01, 0x00, 0x00, 0x00})
+	}
+	if tempTx.Vin[0].Txid == "f3898029a8699bd8b71dc6f20e7ec2762a945a30d6a9f18034ce92a9d6cdd26c" {
+		fmt.Println("serializedTx", serializedTx)
 	}
 	return chainhash.DoubleHashB(serializedTx)
 }
@@ -282,6 +287,48 @@ func SegwitSerializeAllAnyOne(tempTx Transaction, inp TxIn, sigHash []byte) []by
 		outputBytes.Set(encoding.CompactSize(uint64(len(scriptPubKey))))
 		outputBytes.SetBytes(scriptPubKey, false)
 	}
+
+	preImage.SetBytes(chainhash.DoubleHashB(outputBytes.GetBuffer()), false)
+	preImage.Set(tempTx.Locktime)
+
+	preImage.SetBytes(sigHash, false) // sighash
+
+	return preImage.GetBuffer()
+}
+
+func SegwitSerializeSingleAnyOne(tempTx Transaction, pos int, inp TxIn, sigHash []byte) []byte {
+	preImage := encoding.NewLEBuffer()
+
+	preImage.Set(tempTx.Version)
+
+	preImage.SetBytes(MustHexDecode("0000000000000000000000000000000000000000000000000000000000000000"), false) // hashPrevouts
+	preImage.SetBytes(MustHexDecode("0000000000000000000000000000000000000000000000000000000000000000"), false) // hashSequence
+
+	preImage.SetBytes(MustHexDecode(inp.Txid), true)
+	preImage.Set(inp.Vout)
+
+	pubKeyHash := MustHexDecode(inp.Prevout.ScriptPubKey[4:])
+
+	scriptCode := make([]byte, 0)
+	scriptCode = append(scriptCode, []byte{
+		0x19, 0x76, 0xa9, 0x14,
+	}...)
+
+	scriptCode = append(scriptCode, pubKeyHash...)
+	scriptCode = append(scriptCode, 0x88, 0xac)
+
+	preImage.SetBytes(scriptCode, false)
+	preImage.Set(inp.Prevout.Value)
+	preImage.Set(inp.Sequence)
+
+	outputBytes := encoding.NewLEBuffer()
+
+	output := tempTx.Vout[pos]
+
+	outputBytes.Set(output.Value)
+	scriptPubKey := MustHexDecode(output.ScriptPubKey)
+	outputBytes.Set(encoding.CompactSize(uint64(len(scriptPubKey))))
+	outputBytes.SetBytes(scriptPubKey, false)
 
 	preImage.SetBytes(chainhash.DoubleHashB(outputBytes.GetBuffer()), false)
 	preImage.Set(tempTx.Locktime)
